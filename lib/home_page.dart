53c81f13781af000377faab849d420a429a0bd1e
Valerii Gassiev <vvv.gassiev@gmail.com>
2022-12-24 10:39:02 +0300

    commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,2 +1,33 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final List myTiles = [
+    'A',
+    'B',
+    'C',
+    'D',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drag & Drop List'),
+      ),
+      body: ReorderableListView(
+        children: [
+          for (final tile in myTiles)
+        ],
+
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
